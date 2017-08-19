13f08912017fc754383459683d446fb01b79e9a5
Kevin Benard <mail@kevinbenard.com>
2017-08-19 09:51:52 -0600

    Adding calculation of price_per_unit and some validation

--- lib/price_tracker/tracker/tracked_product.ex
+++ lib/price_tracker/tracker/tracked_product.ex
@@ -10,6 +10,7 @@ defmodule PriceTracker.Tracker.TrackedProduct do
     field :price, :float
     field :product, :string
     field :unit, :string
+    field :price_per_unit, :float
 
     timestamps()
   end
@@ -19,5 +20,23 @@ defmodule PriceTracker.Tracker.TrackedProduct do
     tracked_product
     |> cast(attrs, [:product, :date, :amount, :unit, :price])
     |> validate_required([:product, :date, :amount, :unit, :price])
+    |> validate_number(:amount, greater_than: 0.0, message: "You can't purchase zero things.")
+    |> validate_length(:unit, min: 1)
+    |> validate_length(:product, min: 1)
+    |> calculate_price_per_unit
+  end
+
+  defp calculate_price_per_unit(changeset) do
+      amount = get_field(changeset, :amount)
+      price = get_field(changeset, :price)
+
+      if (amount || price) && amount > 0.0 do
+          price_per_unit = price / amount
+
+          changeset
+          |> put_change(:price_per_unit, price_per_unit)
+      else
+          changeset
+      end
   end
 end
